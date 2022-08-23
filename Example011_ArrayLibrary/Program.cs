3be2b1018b5c27de06852d3b6ec7aa54f19f83dd
Alexander Shushlin <alexshushlin@gmail.com>
2022-08-23 18:19:15 +0300

    add Example 011 Array Library

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,51 @@
+﻿// Метод заполнения массива
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+// Метод печати массива на экран
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int indexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    // Искусственно на случай отсутствия нужного числа, чтоб не выводил 0
+    int position = -1;
+    
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = indexOf(array, 4);
+Console.WriteLine(pos);
+
